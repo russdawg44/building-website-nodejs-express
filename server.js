228013238290e4d31f90f46aad446ceff5140845
Russell Hayward <russellhayward@Russells-MacBook-Pro-730.local>
2023-04-09 19:01:49 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
